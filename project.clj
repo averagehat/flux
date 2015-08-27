73b197ca8b0de1025b1b4e925b1923ce3ad2d353
Matt Hoffman <matt.hoffman@lucidworks.com>
2015-08-27 00:13:27 -0400

    update to latest clojure and Solr

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
             :url "https://github.com/mwmitchell/flux"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.6.0"]
-                           [org.apache.solr/solr-core "5.2.1"]
-                           [org.apache.solr/solr-solrj "5.2.1"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
+                           [org.apache.solr/solr-core "5.3.0"]
+                           [org.apache.solr/solr-solrj "5.3.0"]
                            [javax.servlet/servlet-api "2.5"]]
 
             :plugins [[lein-ancient "0.6.5"]]
