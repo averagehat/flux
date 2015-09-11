abcd6b1baf48f409b101de037d5425df898c2d31
Mike Panciera <michael.panciera.work@gmail.com>
2015-09-11 19:38:20 -0400

    Update project.clj
    changing solr version

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0"]
-                           [org.apache.solr/solr-core "5.3.0"]
-                           [org.apache.solr/solr-solrj "5.3.0"]
+                           [org.apache.solr/solr-solrj "4.10.4"]
+                           [org.apache.solr/solr-core "4.10.4"]
                            [javax.servlet/servlet-api "2.5"]]
 
             :plugins [[lein-ancient "0.6.5"]]
